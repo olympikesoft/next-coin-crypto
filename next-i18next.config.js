45c9ce08865c0cdeea42a3594771484607cebd6d
Andre Rodrigues <30387179+olympikesoft@users.noreply.github.com>
2025-12-12 14:35:58 +0000

    fix i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ar'],
+    // I added 'ar' because I saw the folder in your screenshot
+    locales: ['en', 'ar'], 
   },
+  // THIS IS THE FIX:
+  localePath: path.resolve('./public/locales'),
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
